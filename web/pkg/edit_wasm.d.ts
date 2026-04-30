7bf23085ea5e6b189eb063cd56062bf61afefa5d
Broken Pipe <navigaid@gmail.com>
2026-05-01 04:39:36 +0800

    deploy to vercel

---
+++ web/pkg/edit_wasm.d.ts
@@ -0,0 +1,60 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Initialise the editor for a terminal with the given dimensions.
+ *
+ * Must be called exactly once before any other function.
+ * Returns the initial VT bytes to write to xterm.js.
+ */
+export function editor_init(cols: number, rows: number): string;
+
+/**
+ * Notify the editor that the terminal has been resized.
+ *
+ * Returns VT bytes to write to xterm.js.
+ */
+export function editor_resize(cols: number, rows: number): string;
+
+/**
+ * Feed VT-encoded input bytes (keyboard / mouse / paste) to the editor.
+ *
+ * Returns VT bytes to write to xterm.js.
+ */
+export function editor_step(input: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly editor_init: (a: number, b: number, c: number) => void;
+    readonly editor_resize: (a: number, b: number, c: number) => void;
+    readonly editor_step: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_export: (a: number, b: number) => number;
+    readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_export3: (a: number) => void;
+    readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+    readonly __wbindgen_export4: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
